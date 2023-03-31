c205c51a9a1a58d3005c5515ca35f79f28ec0861
Riichan04 <22130274@st.hcmuaf.edu.vn>
2023-04-01 02:58:41 +0700

    some test

--- JS/musicBox.js
+++ JS/musicBox.js
@@ -124,5 +124,19 @@ function checkPlayedMusic() {
         // document.getElementById("nameMusic").innerHTML = `Chuẩn bị phát ${nameAlbum[ranIndex]}`
     }
 }
+
+//Add List Tên Nhạc ở WebCon
+//Add List Tên Nhạc 
+function addJPName() {
+    var jpName = document.getElementById("JPMusic")
+    for (var i = 0; i < musicList.length; i++) {
+        var btnName = document.createElement("p")
+        btnName.id = `JPName`
+        btnName.innerHTML = `${i+1}. ${nameAlbum[i]}`
+        jpName.appendChild(btnName)
+    }
+}
+
+addJPName()
 randomMusic()
 checkPlayedMusic()
--- SubPage/MusicList/JS/musicBox.js
+++ SubPage/MusicList/JS/musicBox.js
@@ -128,7 +128,7 @@ function addJPName() {
     for (var i = 0; i < musicList.length; i++) {
         var btnName = document.createElement("p")
         btnName.id = `JPName`
-        btnName.innerHTML = nameAlbum[i]
+        btnName.innerHTML = `${i+1}. ${nameAlbum[i]}`
         jpName.appendChild(btnName)
     }
 }
