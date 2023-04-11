93e945dee883afad2eccfb1474c5688a59413a9d
Riichan04 <22130274@st.hcmuaf.edu.vn>
2023-04-11 09:39:56 +0700

    update userPage

---
+++ JS/loginCheck.js
@@ -0,0 +1,18 @@
+//Check xem đăng nhập chưa
+var signed = document.getElementsByClassName("signined")
+if (localStorage.length > 0) {
+    console.log(localStorage)
+    signed[1].style.display = "none"
+    signed[0].style.display = "none"
+    const userPopup = document.getElementById("userPopup")
+    userPopup.style.display = ""
+    userPopup.innerHTML = `<i class="fa-regular fa-user icon-header"></i> <u class="headlistitem_item usertext", style="text-decoration: underline;"">${JSON.parse(localStorage.getItem('info')).username}</u>`
+}
+else {
+    signed[1].style.display = ""
+    signed[0].innerHTML = `<li class="headlistitem signined">
+        <i class="fa-solid fa-arrow-right-to-bracket"></i>
+            <a href="SubPage/Login/loginPage.html" class="headlistitem_item"> Đăng Nhập
+            </a>
+        </li>`
+}
