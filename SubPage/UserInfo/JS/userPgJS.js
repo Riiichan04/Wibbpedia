b7f0f45f585ce190c2a4d02f46aa8d30763c72b9
Riichan04 <22130274@st.hcmuaf.edu.vn>
2023-04-17 18:02:44 +0700

    complete change avatar

--- SubPage/UserInfo/JS/userPgJS.js
+++ SubPage/UserInfo/JS/userPgJS.js
@@ -1,62 +1,126 @@
+//InfoBox
 const avtBox = document.getElementById("infoBox_avatar")
 const nickName = document.getElementById("nickName")
 const userName = document.getElementById("userName")
-const confirmchangeAVT = document.getElementById("confirmChangeAvt")
-const changeavt = document.getElementById("changeavt")
-const result = document.getElementById("result")
 
+//List BTN
+const changeAVT = document.getElementById("changeAVT")
+const changeUN = document.getElementById("changeUN")
+const changeSN = document.getElementById("changeSN")
+const changePASS = document.getElementById("changePASS")
+
+//Nền khi mở popup
+const popupFilter = document.getElementById("popupFilter")
+//ListPopup
+const popupAVT = document.getElementById("popupAVT")
+const popupUN = document.getElementById("popupUN")
+const popupSN = document.getElementById("popupSN")
+const popupPASS = document.getElementById("popupPASS")
+
+//PopupAVT
+const inputText = document.getElementById("inputText")
+const errAVT = document.getElementById("errAVT")
+const previewAvt = document.getElementById("previewAvt")
+const notice = document.getElementById("notice")
+
+//List BTN
+const cancelAVT = document.getElementById("cancelAVT")
+const confirmAVT = document.getElementById("confirmAVT")
+
+//Nạp hình và thông tin vào userbox
 avtBox.src = JSON.parse(localStorage.getItem('info')).avatar
 userName.innerHTML = JSON.parse(localStorage.getItem('info')).username
 nickName.innerHTML ="@" + JSON.parse(localStorage.getItem('info')).name
 
-confirmchangeAVT.onclick = async() => {
-    if (localStorage.length > 0) {
-        const myJSON = getAPILogin(1)
+//Mở popup thayAVT
+changeAVT.addEventListener('click', () => {
+    popupFilter.style.display = ""
+    popupAVT.style.display = ""
+    previewAvt.src = JSON.parse(localStorage.getItem('info')).avatar
+})
 
-        for (var i = 0; i < myJSON.length; i++) {
-            if (JSON.parse(localStorage.getItem('info')).username == myJSON[i]["username"]) {
-                res = i
-            }
-        }
+//Đóng popup thayAVT
+cancelAVT.addEventListener('click', () => {
+    popupFilter.style.display = "none"
+    popupAVT.style.display = "none"
+    previewAvt.src = JSON.parse(localStorage.getItem('info')).avatar
+    notice.innerText = ""
+})
 
-        var putJSON = {
-            "avatar": changeavt.value.trim()
-        }
-        getAPILogin(4, putJSON)
-        result.innerHTML = "Đổi avatar thành công"
+let url = inputText.value.trim()
+//Check đường dận nhập vào
+var isAvatar = false
+var isLoaded = false
+inputText.oninput = async () => {
+    isLoaded = false //Mỗi lần nhập input sẽ quay lại false
+    previewAvt.addEventListener('load', () => {
+        isLoaded = true
+    })
+    if (await isImage(inputText.value.trim())) {
+        isAvatar = true
+        previewAvt.src = inputText.value.trim()
+        errAVT.textContent = ""
+    }
+    else {
+        isAvatar = false
+        errAVT.textContent = "URL không hợp lệ! Hãy nhập URL hợp lệ."
     }
 }
 
+const nickN = JSON.parse(localStorage.getItem('info')).name
+const userN = JSON.parse(localStorage.getItem('info')).username
 
-//Tạo token để lấy API ra
-async function getAPILogin(type, x) {
-    var value
-    if (type == 1) {
-        await fetch("https://643306c5d0127730d2dfbfe7.mockapi.io/UserOperator", {
+
+let id = 0
+async function checkID() {
+    const myJSON = await getAPILogin()
+    for (let i = 0; i < myJSON.length; i++) {
+        if (myJSON[i]["username"] == userN) {
+            id = i+1
+        }
+    }
+}
+//Xác nhận thayAVT
+confirmAVT.addEventListener('click', () => {
+    if (inputText.value.trim() == "") {
+        errAVT.textContent = "Chưa có URL!"
+    }
+    else if (isAvatar == false || isLoaded == false) {
+        errAVT.textContent = "URL bạn nhập không phải hình ảnh!"
+    }
+    else {
+        errAVT.textContent = ""
+        notice.textContent = "Thay avatar thành công!"
+        updateAvatarAPI(id, inputText.value.trim())
+        isAvatar = false
+        var updateJSON = {
+            "name": nickN,
+            "username": userN,
+            "avatar": inputText.value.trim()
+        }
+        inputText.value = ""
+        setTimeout(() => {
+            localStorage.setItem('info', JSON.stringify(updateJSON))
+            popupFilter.style.display = "none"
+            popupAVT.style.display = "none"
+            userAVT.src = JSON.parse(localStorage.getItem('info')).avatar
+            previewAvt.src = JSON.parse(localStorage.getItem('info')).avatar
+            avtBox.src = JSON.parse(localStorage.getItem('info')).avatar
+        }, 1500)
+    }
+})
+
+//Gọi API để get thông tin
+async function getAPILogin() {
+    await fetch("https://643306c5d0127730d2dfbfe7.mockapi.io/UserOperator", {
             method: 'GET',
             headers: { 'content-type': 'application/json'}
-        }).then(res => res.json().then(data => {
-            value = data
-        }))
-    }
-    if (type == 2) {
-        await fetch("https://643306c5d0127730d2dfbfe7.mockapi.io/UserOperator", {
-            method: 'POST',
-            headers: { 'content-type': 'application/json'},
-            body: JSON.stringify(x)
-        }).then(res => res.json().then(data => {
-            value = data
-        }))
-    }
-    if (type == 3) {
-        await fetch("https://643306c5d0127730d2dfbfe7.mockapi.io/UserOperator", {
-            method: 'DELETE'
-        }).then(res => res.json().then(data => {
-            value = data
-        }))
-    }
+        })
+        .then(res => res.json())
+        .then(data => {value = data})
     return value
 }
+
 
 //Gọi API để update thông tin
 async function updateAvatarAPI(id, url) {
@@ -69,3 +133,21 @@ async function updateAvatarAPI(id, url) {
     .then(res => res.json())
     .then(data => {value = data})
 }
+
+function checkString() {
+    if (inputText.value.trim() == "") {
+        previewAvt.src = JSON.parse(localStorage.getItem('info')).avatar
+    }
+}
+
+function isImage(url) {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.onload = () => resolve(true)
+      img.onerror = () => resolve(false)
+      img.src = url;
+    });
+}
+
+checkID()
+checkString()
